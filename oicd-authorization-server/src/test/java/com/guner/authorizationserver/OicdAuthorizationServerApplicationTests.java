913951fa517e882f40908498640c4011b08e117b
Ramazan Guner <ramazan.guner@haud.com>
2023-12-26 19:56:47 +0300

    first commit

---
+++ oicd-authorization-server/src/test/java/com/guner/authorizationserver/OicdAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guner.authorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OicdAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ oicd-social-login-client/src/test/java/com/guner/socialloginclient/SocialLoginClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guner.socialloginclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
